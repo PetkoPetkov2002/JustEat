3b70edb059049a315464d068fd3be37b14370a7b
Petko <petkopetkov2002@yahoo.com>
2025-03-27 20:44:57 +0000

    feat:Added upper and lower bound tests for ensuring postcode is within the 5-7 character range

--- ktor-sample/src/test/kotlin/ApplicationTest.kt
+++ ktor-sample/src/test/kotlin/ApplicationTest.kt
@@ -1,17 +1,16 @@
 package com.example
 
 import com.example.model.RestaurantInfo
+import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
+import junit.framework.TestCase.assertFalse
 import kotlinx.serialization.json.Json
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotEquals
-import kotlin.test.assertTrue
+import kotlin.test.*
 
 class ApplicationTest {
 
@@ -52,6 +51,62 @@ class ApplicationTest {
         // Verify response content
         assertNotEquals(0, restaurants.size)
         assertTrue(restaurants.size <= 10)
+    }
+
+    @Test
+    fun testRestaurantEndpointOutputStructure() = testApplication {
+        application {
+            configureRouting()
+            configureSerialization()
+        }
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+        // Make the request and get the response
+        val response = client.get("/restaurants/LL574BB") {
+            accept(ContentType.Application.Json)
+        }
+        //Retrieve the body and deserialize to RestaurantInfo
+        val model_response: List<RestaurantInfo> = response.body()
+
+        // Verify it's an array of RestuarantInfo Objects
+       assertIs<List<RestaurantInfo>>(model_response)
+
+    }
+
+    @Test
+    fun testPostCodeLowerBoundary() = testApplication {
+        application {
+            configureRouting()
+            configureSerialization()
+            configureMonitoring()
+        }
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+        val response = client.get("/restaurants/LL")
+        assertEquals(HttpStatusCode.BadRequest, response.status,message = "Postcode must be between 5-7 characters")
+
+
+    }
+    @Test
+    fun testPostCodeUpperBoundary() = testApplication {
+        application {
+            configureRouting()
+            configureSerialization()
+            configureMonitoring()
+        }
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+        val response = client.get("/restaurants/LL574BBB")
+        assertEquals(HttpStatusCode.BadRequest, response.status,message = "Postcode must be between 5-7 characters")
     }
 
 }
